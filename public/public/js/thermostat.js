cfb28ca6fa546a94e87ba0f33907edd87a5b385b
Dan Mandle <web@dan.mandle.me>
2014-09-24 20:46:09 -0600

    now dom

--- public/public/js/thermostat.js
+++ public/public/js/thermostat.js
@@ -1,10 +1,17 @@
 
+$(function() {
+  // Handler for .ready() called.
+	currentStatus();
+});
+
 function currentStatus(){
+	console.log('yo')
 	$.ajax({
 		type: 'GET',
 		url: 'http://localhost:3000/thermostat'
 	})
 	.success(function(data){
+
 		console.log(data);
 	})
 }
